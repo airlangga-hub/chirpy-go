c9f200bf93af2c6ce03ca5de4065eff608444e4c
Airlangga <airlanggakrishna@gmail.com>
2025-11-03 09:43:22 +0700

    add database

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
+import _ "github.com/lib/pq"
+
 import (
 	"log"
 	"net/http"
 	"sync/atomic"
+	"database/sql"
+	"os"
+	"github.com/joho/godotenv"
+	"github.com/airlangga-hub/chirpy-go/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db *database.Queries
 }
 
 func main() {
@@ -16,8 +23,23 @@ func main() {
 		port = "8080"
 	)
 
+	godotenv.Load()
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
+
+	dbConn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error opening database: %s", err)
+	}
+
+	dbQueries := database.New(dbConn)
+
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		db: dbQueries,
 	}
 
 	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
