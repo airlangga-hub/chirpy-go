211d7e2b8d554a2cfbba11829f860ff1fcba7a79
Airlangga <airlanggakrishna@gmail.com>
2025-11-05 01:19:28 +0700

    add POST /api/chirps endpoint to create chirps

---
+++ handler_chirps_create.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+	"github.com/airlangga-hub/chirpy-go/internal/database"
+	"errors"
+	"time"
+	"github.com/google/uuid"
+)
+
+type Chirp struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Body      string    `json:"body"`
+	UserID    uuid.UUID `json:"user_id"`
+}
+
+func (cfg *apiConfig) handlerChirpsCreate(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+		UserID uuid.UUID `json:"user_id"`
+	}
+
+	var params parameters
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&params); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+
+	cleaned, err := validateChirp(params.Body)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err.Error(), err)
+		return
+	}
+
+	chirp, err := cfg.db.CreateChirp(r.Context(), database.CreateChirpParams{
+		Body: cleaned,
+		UserID: params.UserID,
+	})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create chirp", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, Chirp{
+		chirp.ID,
+		chirp.CreatedAt,
+		chirp.UpdatedAt,
+		chirp.Body,
+		chirp.UserID,
+	})
+}
+
+func validateChirp(body string) (string, error) {
+	const maxChirpLength = 140
+	if len(body) > maxChirpLength {
+		return "", errors.New("Max chirp length 140 characters exceeded")
+	}
+
+	badWords := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert": {},
+		"fornax": {},
+	}
+
+	cleaned := getCleanedBody(body, badWords)
+
+	return cleaned, nil
+}
+
+func getCleanedBody(body string, badWords map[string]struct{}) string {
+	words := strings.Fields(body)
+
+	for i, word := range words {
+		lowerCase := strings.ToLower(word)
+
+		if _, exist := badWords[lowerCase]; exist {
+			words[i] = "****"
+		}
+	}
+
+	return strings.Join(words, " ")
+}
