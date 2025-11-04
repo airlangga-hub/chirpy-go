eacd24f0476a82988b160727f7eb1a102e2c055b
Airlangga <airlanggakrishna@gmail.com>
2025-11-05 02:56:37 +0700

    add GetChirp to get a single chirp by chirpID

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"net/http"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
+func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
 	dbChirps, err := cfg.db.GetChirps(r.Context())
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't get chirps", err)
@@ -23,4 +24,27 @@ func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
 	}
 
 	respondWithJSON(w, http.StatusOK, chirps)
+}
+
+func (cfg *apiConfig) handlerChirpGet(w http.ResponseWriter, r *http.Request) {
+	chirpIDString := r.PathValue("chirpID")
+	chirpID, err := uuid.Parse(chirpIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid chirp ID", err)
+		return
+	}
+
+	chirpDb, err := cfg.db.GetChirp(r.Context(), chirpID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't get chirp", err)
+	return
+	}
+
+	respondWithJSON(w, http.StatusOK, Chirp{
+		chirpDb.ID,
+		chirpDb.CreatedAt,
+		chirpDb.UpdatedAt,
+		chirpDb.Body,
+		chirpDb.UserID,
+	})
 }
